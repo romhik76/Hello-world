74b2f749a99e99b0d68ab525bbdbe999611f37f8
romhik76 <64015552+romhik76@users.noreply.github.com>
2021-09-24 13:14:48 +0300

    created_calculatior_circle

---
+++ task_2/calculator_circle.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b float32
+	fmt.Print("Введите первое число:")
+	fmt.Scanln(&a)
+
+	fmt.Print("Введите второе число:")
+	fmt.Scanln(&b)
+	
+	fmt.Printf("S(площадь фигуры) равна: %f\n", a*b)
+}
+
