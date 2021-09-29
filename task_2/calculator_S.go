15b0fd9c70952372701a64cf8aa820e0da73332b
romhik76 <64015552+romhik76@users.noreply.github.com>
2021-09-29 16:45:03 +0300

    numbers

--- task_2/calculator_S.go
+++ task_2/calculator_S.go
@@ -2,14 +2,18 @@ package main
 
 import "fmt"
 
+
+
 func main() {
-	var a, b float32
-	fmt.Print("Введите первое число:")
-	fmt.Scanln(&a)
+	{
+		var a, b float32
+		fmt.Print("Введите первое число:")
+		fmt.Scanln(&a)
 
-	fmt.Print("Введите второе число:")
-	fmt.Scanln(&b)
-	
-	fmt.Printf("S(площадь фигуры) равна: %f\n", a*b)
+		fmt.Print("Введите второе число:")
+		fmt.Scanln(&b)
+
+		fmt.Println("S(площадь фигуры) равна:", a*b)
+	}
+
 }
-
--- task_2/calculator_numbers.go
+++ task_2/calculator_numbers.go
@@ -4,12 +4,13 @@ import "fmt"
 
 func main() {
 	var a, b float32
+
 	fmt.Print("Введите первое число:")
 	fmt.Scanln(&a)
 
 	fmt.Print("Введите второе число:")
 	fmt.Scanln(&b)
-	
+
 	fmt.Printf("S(площадь фигуры) равна: %f\n", a*b)
 }
 
